3791507d29c6e5decda9397a9c03f6003051667d
hyphen <lhfcjhyy@gmail.com>
2014-08-23 12:58:33 +0800

    commit

---
+++ init.el
@@ -0,0 +1,11 @@
+;; -*- coding: utf-8 -*-
+
+;;;; load base settings
+(add-to-list 'load-path "~/.emacs.d/wttr-config")
+(require 'wttr-font)
+(require 'wttr-basic)
+(require 'wttr-key)
+(require 'wttr-color-theme)
+(require 'wttr-mode-line)
+(require 'wttr-modes)
+

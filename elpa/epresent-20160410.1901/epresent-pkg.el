f232cc0516d8020dcbde0efa17f56aa489efa375
hyphen <lhfcjhyy@gmail.com>
2016-07-27 09:45:45 +0800

    update windows

---
+++ elpa/epresent-20160410.1901/epresent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "epresent" "20160410.1901" "Simple presentation mode for Emacs Org-mode" '((org "8") (cl-lib "0.5")) :url "https://github.com/dakrone/epresent" :keywords '("gui"))
---
+++ elpa/org-tree-slide-20160513.2325/org-tree-slide-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-tree-slide" "20160513.2325" "A presentation tool for org-mode" 'nil :keywords '("org-mode" "presentation" "narrowing"))
